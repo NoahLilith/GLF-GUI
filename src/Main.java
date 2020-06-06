e830d99cd5e05e8e93b5607cf5bebc78c420bc39
NoahLilith <64060047+NoahLilith@users.noreply.github.com>
2020-06-06 12:57:15 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame demo = new JFrame("GoodLifeFood");
+        demo.setSize(300, 300);
+        demo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        demo.setVisible(true);
+    }
+}
